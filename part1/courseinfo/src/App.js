448d64a424e55e8f681d0eee5c963469451eea59
Logn Xu <lixu@lakala.com>
2024-03-14 21:49:12 +0800

    init  courseinfo

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Header = (props) => {
+  console.log(props);
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.name} exercises={props.exercises} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content {...course.parts[0]} />
+      <Content {...course.parts[1]} />
+      <Content {...course.parts[2]} />
+      <Total
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+
+// const refresh = () => {
+//     ReactDOM.createRoot(document.getElementById('root')).render(
+//         <App counter={counter} />
+//     )
+// }
+
+
+// setInterval(() => {
+//     refresh()
+//     counter += 1
+// }, 1000);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
